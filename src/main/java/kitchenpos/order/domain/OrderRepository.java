332942dd4fcc3d2764ea1d6f105962cfa1f41d65
Donyub Park <ninjasul@gmail.com>
2024-05-24 09:53:55 +0900

    Step3: 기능 우선 패키지 구성하기 (#393)
    * refactor: add menu, product value objects
    
    * refactor: refactor ProductService
    
    * refactor: add NumberOfGuest value object
    
    * refactor: refactor MenuService
    
    * test: add MenuTest
    
    * feat: add DeliveryAddress value object
    
    * refactor: refactor Order to apply DeliveryAddress value object
    
    * test: change display name
    
    * feat: split Order into DeliverOrder, TakeoutOrder and EatInOrder
    
    * refactor: refactor OrderService
    
    * refactor: restructures packages
    
    * bugfix: change access modifier of constructor
    
    * refactor: move DTO classes to application packages
    
    * refactor: move repository classes to domain packages
    
    ---------
    
    Co-authored-by: dongyub.park <dongyub.park@42dot.ai>

---
+++ src/main/java/kitchenpos/order/domain/OrderRepository.java
@@ -0,0 +1,20 @@
+package kitchenpos.order.domain;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.OrderStatus;
+import kitchenpos.order.domain.OrderTable;
+
+public interface OrderRepository {
+    Order save(Order order);
+
+    Optional<Order> findById(UUID id);
+
+    List<Order> findAll();
+
+    boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status);
+}
+
